144d10cc13819ae1d9dd0b9b173d87a5e5e5c61f
Paco Pronk <paco.pronk@fabrique.nl>
2026-03-11 09:45:17 +0100

    Convert position logic to angular logic for haply device

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from classes.PandaWorld3D import PandaWorld3D
 from classes.HapticDevice import HapticDevice
 
 MOUSE_SENSITIVITY = 0.15
-HAPTIC_SCALE = 300  # degrees per meter
+HAPTIC_SCALE = 1.0  # aim degrees per motor degree
 
 haptic = HapticDevice()
+if haptic.connected:
+    haptic.calibrate()
 world = PandaWorld3D()
 
 aim_yaw = 0.0
@@ -14,9 +16,10 @@ while True:
     world.taskMgr.step()
 
     if haptic.connected:
-        x, y = haptic.get_position()
-        aim_yaw = x * HAPTIC_SCALE
-        aim_pitch = y * HAPTIC_SCALE
+        a1, a2 = haptic.get_angles()
+        haptic.set_force(0, 0)
+        aim_pitch   = (a1 - a2) * HAPTIC_SCALE
+        aim_yaw = (-a1 - a2) * HAPTIC_SCALE
     else:
         dx, dy = world.get_mouse_delta()
         aim_yaw -= dx * MOUSE_SENSITIVITY
